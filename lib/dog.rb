b0d391d3c965a4ae4ba66f0e8cd1054ac767d896
Daniel Escalona <daniel07escalona@gmail.com>
2022-06-09 13:04:38 -0400

    commit tree b61700d58bc92c0de2e2b3087eddd17bda6ce1ae

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,9 @@
 class Dog
+  attr_accessor :name
+  attr_accessor :breed
 
+  def initialize(name="dani", breed="chihu")
+    @name = name
+    @breed = breed
+  end
 end
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,9 @@
 class Person
+  attr_accessor :name
+  attr_accessor :job
 
+  def initialize(name="john", job="developer")
+    @name = name
+    @job = job
+  end
 end
